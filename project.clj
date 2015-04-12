0f078d1fdf63c5b951cca8d5bf51782a9e40269a
siscia <simone@mweb.biz>
2015-04-12 05:34:05 +0200

    add the project.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject html-generator "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [net.sourceforge.cssparser/cssparser "0.9.15"]
+                 [hickory "0.5.4"]
+                 [instaparse "1.3.6"]])
