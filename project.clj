990cdf05b7c6cd138a3e2dd61554402278bb1275
siscia <simone@mweb.biz>
2015-05-11 14:50:56 +0200

    drop useless deps, change ns per test, better counter

--- project.clj
+++ project.clj
@@ -4,6 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [net.sourceforge.cssparser/cssparser "0.9.15"]
-                 [hickory "0.5.4"]
                  [instaparse "1.3.6"]])
