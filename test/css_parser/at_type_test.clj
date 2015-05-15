673543d05cb58e0cf6f0fac9a466b49729947e89
siscia <simone@mweb.biz>
2015-05-15 11:21:51 +0200

    clean up some mess around... break test

--- test/css_parser/at_type_test.clj
+++ test/css_parser/at_type_test.clj
@@ -40,31 +40,31 @@
 (deftest media-test
   (testing "media key"
     (is (= (p/parse gp "@media screen { body { font-size: 13px }}")
-[:debug [:at-rules [:media "@media " "screen" [:rule [:selector [:relationship-selector [:nested [:primitive-selector [:html-element "body"]]]]] [:token [:font-size "font-size" [:font-size-value [:length [:integer "13"] "px"]]]]]]]]))
-    (is (= (p/parse gp "@media screen,print {
-                               body { font-size: 13px;
-                                      color: red }
-                               body { line-height: 1.2 }}")
-           [:debug [:at-rules [:media "@media " "screen" "print" [:rule [:selector [:relationship-selector [:nested [:primitive-selector [:html-element "body"]]]]] [:token [:font-size "font-size" [:font-size-value [:length [:integer "13"] "px"]]]] [:token [:color "color" [:color-value [:color-type [:color-keyword "red"]]]]]] [:rule [:selector [:relationship-selector [:nested [:primitive-selector [:html-element "body"]]]]] [:token [:line-height "line-height" [:line-height-value [:float "1" "." "2"]]]]]]]]))))
+           [:debug [:at-rules [:media "@media " "screen" [:rule [:selector [:primitive-selector [:html-element "body"]]] [:declarations [:token [:font-size "font-size" [:font-size-value [:length [:integer "13"] "px"]]]]]]]]]))
+    (is (= (p/parse gp "@media screen, print { body { font-size: 13px; color: red } body { line-height: 1.2 }}")
+           [:debug [:at-rules [:media "@media " "screen" "print" [:rule [:selector [:primitive-selector [:html-element "body"]]] [:declarations [:token [:font-size "font-size" [:font-size-value [:length [:integer "13"] "px"]]]] [:token [:color "color" [:color-value [:color-type [:color-keyword "red"]]]]]]] [:rule [:selector [:primitive-selector [:html-element "body"]]] [:declarations [:token [:line-height "line-height" [:line-height-value [:float "1" "." "2"]]]]]]]]]))))
 
 (deftest page-test
   (testing "at page"
     (is (= (p/parse gp "@page :first { margin:2in; }")
-           [:debug [:at-rules [:page "@page " ":first" [:token [:margin "margin" [:margin-all [:length [:integer "2"] "in"]]]]]]]))
-    (is (= (p/parse gp "@page :first { margin:2in;
-                                       background: red;}")
-           [:debug [:at-rules [:page "@page " ":first" [:token [:margin "margin" [:margin-all [:length [:integer "2"] "in"]]]] [:token [:background "background" [:background-color-value [:color-type [:color-keyword "red"]]]]]]]]))))
+           [:debug [:at-rules [:page "@page " ":first" [:declarations [:token [:margin "margin" [:margin-all [:length [:integer "2"] "in"]]]]]]]]))
+    (is (= (p/parse gp "@page :first { margin:2in; background: red;}")
+           [:debug [:at-rules [:page "@page " ":first" [:declarations [:token [:margin "margin" [:margin-all [:length [:integer "2"] "in"]]]] [:token [:background "background" [:background-color-value [:color-type [:color-keyword "red"]]]]]]]]]))))
 
 (deftest font-face-test
   (testing "font-face"
     (is (= (p/parse gp "@font-face {
       font-family: \"Bitstream Vera Serif Bold\";
       src: url(\"https://mdn.mozillademos.org/files/2468/VeraSeBd.ttf\");}")
-           [:debug [:at-rules [:font-face "@font-face " "{" [:token [:font-family "font-family" [:family-font-name [:string "Bitstream Vera Serif Bold"]]]] [:token [:src "src" [:src-value [:url "\"https://mdn.mozillademos.org/files/2468/VeraSeBd.ttf\""]]]] "}"]]]))
-    (is (= (p/parse gp "@font-face {
-        font-family: MyHelvetica;
-        src: local(\"Helvetica Neue Bold\"),
-             local(\"HelveticaNeue-Bold\"),
-             url(MgOpenModernaBold.ttf);
-        font-weight: bold;}")
-           [:debug [:at-rules [:font-face "@font-face " "{" [:token [:font-family "font-family" [:family-font-name [:custom-ident "M" "yHelvetica" ""]]]] [:token [:src "src" [:src-value "local(" [:string "Helvetica Neue Bold"] ")" "local(" [:string "HelveticaNeue-Bold"] ")" [:url "MgOpenModernaBold.ttf"]]]] [:token [:font-weight "font-weight" [:font-weight-value "bold"]]] "}"]]]))))
+           [:debug [:at-rules [:font-face "@font-face " "{" [:declarations [:token [:font-family "font-family" [:family-font-name [:string "Bitstream Vera Serif Bold"]]]] [:token [:src "src" [:src-value [:url "\"https://mdn.mozillademos.org/files/2468/VeraSeBd.ttf\""]]]]] "}"]]]))
+    (is (= (p/parse gp "@font-face { font-family: MyHelvetica; src: local(\"Helvetica Neue Bold\"), local(\"HelveticaNeue-Bold\"), url(MgOpenModernaBold.ttf); font-weight: bold;}")
+           [:debug [:at-rules [:font-face "@font-face " "{" [:declarations [:token [:font-family "font-family" [:family-font-name [:custom-ident "M" "yHelvetica" ""]]]] [:token [:src "src" [:src-value "local(" [:string "Helvetica Neue Bold"] ")" "local(" [:string "HelveticaNeue-Bold"] ")" [:url "MgOpenModernaBold.ttf"]]]] [:token [:font-weight "font-weight" [:font-weight-value "bold"]]]] "}"]]]))))
+
+(deftest keyframes-test
+  (testing "keyframes"
+    (is (= (p/parse gp "@keyframes identifier { 0% { top: 0; left: 0; }
+100% { top: 100px; left: 100%; }}")
+           [:debug [:at-rules [:keyframes "@keyframes " [:identifier [:custom-ident "i" "dentifier" ""]] "{" [:percentage [:integer "0"] "%"] "{" [:declarations [:token [:top "top" [:top-value [:length "0"]]]] [:token [:left "left" [:left-value [:length "0"]]]]] "}" [:percentage [:integer "100"] "%"] "{" [:declarations [:token [:top "top" [:top-value [:length [:integer "100"] "px"]]]] [:token [:left "left" [:left-value [:percentage [:integer "100"] "%"]]]]] "}" "}"]]]))
+    (is (= (p/parse gp "@keyframes important1 { from { margin-top: 50px; } 50%  { margin-top: 150px !important; } to   { margin-top: 100px; }}")
+           [:debug [:at-rules [:keyframes "@keyframes " [:identifier [:custom-ident "i" "mportant" "1"]] "{" "from" "{" [:declarations [:token [:margin-top "margin-top" [:margin-top-value [:length [:integer "50"] "px"]]]]] "}" [:percentage [:integer "50"] "%"] "{" [:declarations [:token [:margin-top "margin-top" [:margin-top-value [:length [:integer "150"] "px"]]]] "!important"] "}" "to" "{" [:declarations [:token [:margin-top "margin-top" [:margin-top-value [:length [:integer "100"] "px"]]]]] "}" "}"]]]))))
+
