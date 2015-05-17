284200b9109445ef30d40bf1d28a271d21ce7274
siscia <simone@mweb.biz>
2015-05-17 08:28:49 +0200

    remove useless function

--- src/css_parser/core.clj
+++ src/css_parser/core.clj
@@ -1,47 +1,61 @@
-(ns html-generator.core
+(ns css-parser.core
   (:require [instaparse.core :as p]))
 
-(defn css-parser []
-  (p/parser (str (slurp "rules.bnf")
+(defn css-parser-full-file []
+  (p/parser (str "file = (rule | sh+ | at-rules)+ \n"
+                 (slurp "rules.bnf")
                  "\n"
                  (slurp "types.bnf"))))
 
-(defn parse-css
-  ([parser file]
-     (p/parse parser file))
-  ([parser]
-     (partial parse-css parser))
-  ([]
-     (partial parse-css (css-parser))))
+(defn css-parser-single-rule []
+  (p/parser (str "single-rule = sh? (rule | at-rules) \n"
+                 (slurp "rules.bnf")
+                 "\n"
+                 (slurp "types.bnf"))))
 
-(defn split-file-in-rules [sequence]
+(defn eliminate-comment [string]
+  (let [re (re-pattern #"\*\/(?s)(.*)\*/")]
+    (clojure.string/replace string re "")))
+
+(defn eliminate-blank [string]
+  (clojure.string/replace string #"(\{|;)(\s)" "$1"))
+
+(defn- merge-lines [seq-of-lines]
+  (concat (first seq-of-lines)
+          (lazy-seq
+           (merge-lines (rest seq-of-lines)))))
+
+(defn file-reader->char-seq [f]
+  (let [lines (line-seq f)]
+    (merge-lines lines)))
+
+(defn split-char-seq-in-rules [sequence]
   (loop [s sequence
          counter 0
          buffer (transient [])]
-    (if (= -1 counter) ;; :
+    (if (= -1 counter) ;;
       (cons (apply str (persistent! buffer))
-            (lazy-seq (split-file-in-rules s)))
+            (lazy-seq (split-char-seq-in-rules s)))
       (if (not (seq s)) ;; if the sequence is terminate we return the buffer and nil to indicate the end
         (cons (apply str (persistent! buffer)) nil)
         (let [actual (first s)]
           (case counter
-            0 (recur (drop 1 s) ;; "normal" state
-                     (if (= \{ actual)
-                       1 0)
-                     (conj! buffer actual))
-            -1 (recur (drop 1 s) ;; sequence to cut here
-                      0
-                      (transient []))
-            1 (recur (drop 1 s) ;; possible next cut
+            1 (recur (rest s) ;; possible next cut
                      (case actual
                        \{ (inc counter)
                        \} -1
                        counter)
                      (conj! buffer actual))
-            (recur (drop 1 s)   ;; most generic state
+            0 (recur (rest s) ;; "normal" state
+                     (if (= \{ actual)
+                       1 0)
+                     (conj! buffer actual))
+            -1 (recur (rest s) ;; sequence to cut here
+                      0
+                      (transient []))
+            (recur (rest s)   ;; most generic state
                    (case actual
                      \{ (inc counter)
                      \} (dec counter)
                      counter)
                    (conj! buffer actual))))))))
-
