c9bb36a6c73ee87a8c12288d2a9fcfb0e51e398d
siscia <simone@mweb.biz>
2015-07-27 12:05:33 +0200

    add resource paths

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [instaparse "1.3.6"]])
+                 [instaparse "1.3.6"]]
+
+  :resource-paths ["/resources"])
